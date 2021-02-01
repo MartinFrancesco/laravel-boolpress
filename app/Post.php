3f15e3ae0fcd3bd0cf09e6bb1604fdd2f3f400dc
Francesco <francescomartinmail@gmail.com>
2021-02-01 18:43:04 +0100

    Login

---
+++ app/Post.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    // DB RELATIONS
+
+    // posts - users
+    public function user() {
+        return $this->belongsTo('App/User');
+    }
+}
---
+++ resources/views/guests/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <h1>HOMEPAGE</h1>
+
+    <p>lorem ipsum</p>
+</div>
+@endsection
